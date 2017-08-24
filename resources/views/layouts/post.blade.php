cef86c1232fcd5c4dcc674ffb0fdd4fd7394b507
Reginaldo Junior <reginaldo.junior696@gmail.com>
2017-08-23 22:51:40 -0300

    update metas

--- resources/views/layouts/post.blade.php
+++ resources/views/layouts/post.blade.php
@@ -2,12 +2,12 @@
     $post = \Admin\Models\Post::find(request()->route()->parameters()['child']);
 ?>
 <meta property="og:title" content="{{ $post->title }}">
-<meta property="og:image" content="{{ $post->image }}">
-<meta property="og:description" content="{{ str_limit(strip_tags($post->description), 150) }}">
+<meta property="og:image" content="{{ request()->getSchemeAndHttpHost().$post->image }}">
+<meta property="og:description" content="{{ str_limit(strip_tags($post->body), 150) }}">
 <meta property="og:url" content="{{ url()->current() }}">
 <meta property="og:type" content="article">
 <meta name="twitter:card" content="summary">
 <meta name="twitter:url" content="{{ url()->current() }}">
 <meta name="twitter:title" content="{{ $post->title }}">
-<meta name="twitter:image" content="{{ $post->image }}">
-<meta name="twitter:description" content="{{ str_limit(strip_tags($post->description), 150) }}">
+<meta name="twitter:image" content="{{ request()->getSchemeAndHttpHost().$post->image }}">
+<meta name="twitter:description" content="{{ str_limit(strip_tags($post->body), 150) }}">
--- resources/views/layouts/schedule.blade.php
+++ resources/views/layouts/schedule.blade.php
@@ -2,12 +2,12 @@
     $schedule = \Admin\Models\Schedule::find(request()->route()->parameters()['child']);      
 ?>
 <meta property="og:title" content="{{ $schedule->name }}">
-<meta property="og:image" content="{{ $schedule->image }}">
+<meta property="og:image" content="{{ request()->getSchemeAndHttpHost().$schedule->image }}">
 <meta property="og:description" content="{{ str_limit(strip_tags($schedule->description), 150) }}">
 <meta property="og:url" content="{{ url()->current() }}">
 <meta property="og:type" content="article">
 <meta name="twitter:card" content="summary">
 <meta name="twitter:url" content="{{ url()->current() }}">
 <meta name="twitter:title" content="{{ $schedule->name }}">
-<meta name="twitter:image" content="{{ $schedule->image }}">
+<meta name="twitter:image" content="{{ request()->getSchemeAndHttpHost().$schedule->image }}">
 <meta name="twitter:description" content="{{ str_limit(strip_tags($schedule->description), 150) }}">
